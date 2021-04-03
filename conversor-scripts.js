b7e1800dcb5cc6f19ef502d01a24ded89347babc
Débora Deotti <81835864+deboradeotti@users.noreply.github.com>
2021-04-03 20:31:56 -0300

    Update conversor-scripts.js

--- conversor-scripts.js
+++ conversor-scripts.js
@@ -2,6 +2,13 @@ function calcular() {
   var entrada = document.querySelector('#anosluz')
   var valorAnosLuz = entrada.value
   var anosLuzFloat = parseFloat(valorAnosLuz)
-  var kmCalcula = (anosLuzFloat*(9.461*(10**12))).toFixed(3)                  
-  document.querySelector(".resultado").innerHTML = "A distância é de " + kmCalcula + " km."
+  var kmCalcula = (anosLuzFloat*(9.461*(10**12))).toFixed(3)  
+  var valida = isNaN(kmCalcula)
+  if (valida === false) {                
+    document.querySelector(".resultado").innerHTML = "A distância é de " + kmCalcula + " km."
+  } 
+  if (valida === true) {
+    document.querySelector(".resultado").innerHTML = "Valor inválido."
+  }
+  entrada.value = ""
   }
